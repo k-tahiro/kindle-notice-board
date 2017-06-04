123f6d04fe95e99e4fd3104bfa0260be46c0ba2e
Keisuke Hirota <tahiro.k.ad@gmail.com>
2017-06-05 00:35:18 +0900

    一時コミット

--- server/routes.py
+++ server/routes.py
@@ -1,3 +1,5 @@
+import email
+
 import boto3
 
 # バケット名
@@ -6,5 +8,32 @@ AWS_S3_BUCKET_NAME = 'tahiro-bucket'
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(AWS_S3_BUCKET_NAME)
 
+
+def _download_latest_mail() -> None:
+    object_summary_iterator = bucket.objects.all()
+
+    target_object = None
+    for os in object_summary_iterator:
+        if target_object is None or (target_object.last_modified < os.last_modified):
+            target_object = os
+
+    bucket.download_file(target_object.key, 'tmpfile')
+
+
+def _extract_attached_file():
+    with open('tmpfile') as f:
+        message = email.message_from_file(f)
+
+    for part in message.walk():
+        if part.get_content_maintype() == 'multipart':
+            continue
+        attach_fname = part.get_filename()
+        if attach_fname:
+            return part.get_payload(decode=True)
+
+    return None
+
+
 def knb():
-    return bucket.get_available_subresources()
+    _download_latest_mail()
+    return _extract_attached_file()
