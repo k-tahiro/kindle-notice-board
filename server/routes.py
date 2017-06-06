2cc486413e7b0bace918086dec22fdebd9635fe3
Keisuke Hirota <tahiro.k.ad@gmail.com>
2017-06-06 22:35:59 +0900

    濃い文字に修正

--- server/routes.py
+++ server/routes.py
@@ -39,7 +39,7 @@ def _extract_attached_file():
 
 
 def _convert_aa():
-    return converter_cli.run('image', custom_char='*').rstrip() + '\n'
+    return converter_cli.run('image', custom_char='W').rstrip() + '\n'
 
 
 def knb():
