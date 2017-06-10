faf53bde95f7a37ed73363a3a9cf6356c6252d9a
Keisuke Hirota <tahiro.k.ad@gmail.com>
2017-06-10 17:52:34 +0900

    不要な出力を抑止

--- server/setup/install-i2a.sh
+++ server/setup/install-i2a.sh
@@ -2,17 +2,17 @@
 
 readonly SCRIPT_DIR=$(dirname "$0")
 
-pushd "${SCRIPT_DIR}/../src"
-git clone https://github.com/xshellinc/image-to-ascii.git > /dev/null
+pushd "${SCRIPT_DIR}/../src" > /dev/null 2>&1
+git clone https://github.com/xshellinc/image-to-ascii.git > /dev/null 2>&1
 
-pushd image-to-ascii
+pushd image-to-ascii > /dev/null 2>&1
 for py_file in $(ls | grep .py)
 do
-  2to3 -w "${py_file}" > /dev/null
+  2to3 -w "${py_file}" > /dev/null 2>&1
 done
 sed -i 's#/2#//2#g' converter.py
 sed -i 's#pixel_value/color_range#pixel_value//color_range#g' converter.py
 sed -i 's/print(ascii_image)/return ascii_image/g' converter_cli.py
 
-popd
-popd
+popd > /dev/null 2>&1
+popd > /dev/null 2>&1
